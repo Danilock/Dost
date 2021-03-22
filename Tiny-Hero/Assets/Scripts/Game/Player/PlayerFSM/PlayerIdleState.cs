622a221920810cef653c576aefc0e56af8b42d6c
Danilock <yo1234re@gmail.com>
2021-03-22 15:45:28 -0400

    Initial Commit
    Fsm and Player Input

---
+++ Tiny-Hero/Assets/Scripts/Game/Player/PlayerFSM/PlayerIdleState.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.StateMachine;
+
+public class PlayerIdleState : State<Player>
+{
+    public override void EnterState(Player entity)
+    {
+        
+    }
+
+    public override void TickState(Player entity)
+    {
+        
+    }
+
+    public override void ExitState(Player entity)
+    {
+        
+    }
+}
---
+++ Tiny-Hero/Assets/Scripts/Game/Player/PlayerInputHandler.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private PlayerInput _inputActions;
+
+    public Vector2 Move { get; private set; }
+
+    private void Awake() {
+        _inputActions = new PlayerInput();
+        _inputActions.Enable();
+    }
+
+    private void Update() {
+        Move = _inputActions.Player.Move.ReadValue<Vector2>();
+    }
+
+    [ContextMenu("Desactivate Movement")]
+    public void DesactivateMovement() => _inputActions.Player.Move.Disable();
+
+    [ContextMenu("Desactivate Jump")]
+    public void DesactivateJump() => _inputActions.Player.Jump.Disable();
+
+    [ContextMenu("Activate Movement")]
+    public void ActivateMovement() => _inputActions.Player.Move.Enable();
+
+    [ContextMenu("Activate Jump")]
+    public void ActivateJump() => _inputActions.Player.Jump.Enable();
+}
